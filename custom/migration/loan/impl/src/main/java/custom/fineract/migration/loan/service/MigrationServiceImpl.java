9749a66ca2e3d4c49d3ae2face004c7aba0a9cb5
Oleksii Novikov <alexey.novikov@onix-systems.com>
2025-02-27 20:48:15 +0200

    FINERACT-2173: Implement custom serializer to include last dt_migration entry in customData

---
+++ custom/migration/loan/impl/src/main/java/custom/fineract/migration/loan/service/MigrationServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package custom.fineract.migration.loan.service;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import org.apache.fineract.infrastructure.dataqueries.data.GenericResultsetData;
+import org.apache.fineract.infrastructure.dataqueries.service.ReadWriteNonCoreDataService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MigrationServiceImpl implements MigrationService {
+
+    private final ReadWriteNonCoreDataService readWriteNonCoreDataService;
+
+    @Override
+    public Optional<String> findLastStatus(final Long loanId) {
+        final GenericResultsetData resultSet = readWriteNonCoreDataService.retrieveDataTableGenericResultSet("dt_migration", loanId,
+                "id desc", null);
+        if (resultSet != null && !resultSet.getData().isEmpty()) {
+            final Map<String, Integer> columnMap = IntStream.range(0, resultSet.getColumnHeaders().size()).boxed()
+                    .collect(Collectors.toMap(i -> resultSet.getColumnHeaders().get(i).getColumnName(), i -> i));
+
+            final Object status = resultSet.getData().get(0).getRow().get(columnMap.get("status"));
+            return Optional.ofNullable((String) status);
+        }
+
+        return Optional.empty();
+    }
+
+}
